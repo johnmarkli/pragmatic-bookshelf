4b08f6852c27f692c4edb0ac52cf2a6b3e8dbc1d
John Li <johnmarkli@gmail.com>
2012-09-14 15:32:53 -0400

    chapter 9 cart creation

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,19 @@
+class StoreController < ApplicationController
+  def index
+  	# @products = Product.find(:all)
+  	@products = Product.order(:title)
+
+    @session_counter = session[:counter]
+    if @session_counter.nil?
+      @session_counter = 1
+    else
+      @session_counter += 1
+    end
+    session[:counter] = @session_counter
+
+	if @session_counter > 5
+    	@counter_message = "You've accessed this index page #{@session_counter} times"
+    end
+
+  end
+end

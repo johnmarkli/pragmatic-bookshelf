5a745ecce49c57bb5a1328576341fce514e6e1b4
John Li <johnmarkli@gmail.com>
2012-09-20 15:41:36 -0400

    Iteration G1 added Order and test

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,8 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :quantity
+  attr_accessible :cart, :cart_id, :product, :product_id, :quantity
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
 
   def total_price
   	product.price * quantity
